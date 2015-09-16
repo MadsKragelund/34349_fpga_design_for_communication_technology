b2ef271759da736c9ad6ebb1a7dc8bd7b0fa7015
Christian Kjær Laustsen <ckl@codetalk.io>
2015-09-16 15:40:27 +0200

    Remove wrong ;

--- src/crc.vhd
+++ src/crc.vhd
@@ -8,7 +8,7 @@ entity fcs_check_serial is
     start_of_frame : in  std_logic;     -- arrival of the first bit.
     end_of_frame   : in  std_logic;     -- arrival of the first bit in FCS.
     data_in        : in  std_logic;     -- serial input data.
-    fcs_error      : out std_logic;     -- indicates an error.
+    fcs_error      : out std_logic      -- indicates an error.
     );
 end fcs_check_serial;
 
